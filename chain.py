cc24a7c27e3adf2635c15043e5998b7687b93489
Choi Rak_Kyoung <knreipiel@hotmail.com>
2018-06-03 17:07:16 +0900

    genesis block and chain initialize

--- chain.py
+++ chain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
+from pprint import pprint
 import random
+import sys
 from typing import Dict
 
 
@@ -24,6 +26,26 @@ def is_valid_txn(txn: Dict, state: Dict) -> bool:
             return False
 
     return True
+
+
+def make_block(txns: list, chain: list) -> Dict:
+    parent_block = chain[-1]
+    parent_hash = parent_block['hash']
+    block_number = parent_block['contents']['block_number'] + 1
+    txn_count = len(txns)
+    block_contents = {
+        'block_number': block_number,
+        'parent_hash': parent_hash,
+        'txn_count': txn_count,
+        'txns': txns,
+    }
+    block_hash = hash_me(block_contents)
+    block = {
+        'hash': block_hash,
+        'contents': block_contents,
+    }
+
+    return block
 
 
 def make_transaction(max_value: int = 3) -> Dict:
@@ -52,14 +74,62 @@ if __name__ == '__main__':
     state = {'Alice': 5, 'Bob': 5}
 
     # Basic transaction- this works great!
-    print(is_valid_txn({'Alice': -3, 'Bob': 3}, state))  # True
+    pprint(is_valid_txn({'Alice': -3, 'Bob': 3}, state))  # True
     # But we can't create or destroy tokens
-    print(is_valid_txn({'Alice': -4, 'Bob': 3}, state))  # False
+    pprint(is_valid_txn({'Alice': -4, 'Bob': 3}, state))  # False
     # We also can't overdraft our account.
-    print(is_valid_txn({'Alice': -6, 'Bob': 6}, state))  # False
+    pprint(is_valid_txn({'Alice': -6, 'Bob': 6}, state))  # False
     # Creating new users is valid
-    print(is_valid_txn({'Alice': -4, 'Bob': 2, 'Lisa': 2}, state))  # True
+    pprint(is_valid_txn({'Alice': -4, 'Bob': 2, 'Lisa': 2}, state))  # True
     # But the same rules still apply!
-    print(is_valid_txn({'Alice': -4, 'Bob': 3, 'Lisa': 2}, state))  # False
+    pprint(is_valid_txn({'Alice': -4, 'Bob': 3, 'Lisa': 2}, state))  # False
 
-            
+    print('=== Lets make genesis block')
+    state = {'Alice': 50, 'Bob': 50}
+    genesis_block_txns = [state]
+    genesis_block_contents = {
+        'block_number': 0,
+        'parent_hash': None,
+        'txn_count': 1,
+        'txns': genesis_block_txns,
+    }
+    genesis_hash = hash_me(genesis_block_contents)
+    genesis_block = {
+        'hash': genesis_hash,
+        'contents': genesis_block_contents,
+    }
+    genesis_block_str = json.dumps(genesis_block, sort_keys=True)
+
+    chain = [genesis_block]
+    print('chain:')
+    pprint(chain)
+
+    print('=== Lets try to make blocks')
+    # Let's try to make blocks
+    block_size_limit = 5
+
+    while len(txn_buffer) > 0:
+        buffer_start_size = len(txn_buffer)
+
+        txn_list = []
+        while (len(txn_buffer) > 0) and (len(txn_list) < block_size_limit):
+            new_txn = txn_buffer.pop()
+            valid_txn = is_valid_txn(new_txn, state)
+
+            if valid_txn:
+                txn_list.append(new_txn)
+                state = update_state(new_txn, state)
+            else:
+                print('ignored transaction')
+                sys.stdout.flush()
+                continue
+
+    my_block = make_block(txn_list, chain)
+    chain.append(my_block)
+
+    print('chain[0]:')
+    pprint(chain[0])
+    print('chain[1]:')
+    pprint(chain[1])
+    print('state:')
+    pprint(state)
